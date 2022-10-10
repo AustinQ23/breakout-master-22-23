5eacaf60684c23c5ea0948139212c8fa0437de53
Peter Strawn <peter.strawn@jesuithighschool.org>
2022-10-10 11:23:08 -0700

    breakout-10

--- main.lua
+++ main.lua
@@ -111,9 +111,12 @@ function love.load()
         ['play'] = function() return PlayState() end,
         ['serve'] = function() return ServeState() end,
         ['game-over'] = function() return GameOverState() end,
-        ['victory'] = function() return VictoryState() end
+        ['victory'] = function() return VictoryState() end,
+        ['high-scores'] = function() return HighScoreState() end
     }
-    gStateMachine:change('start')
+    gStateMachine:change('start', {
+      highScores = loadHighScores()
+    })
 
     -- a table we'll use to keep track of which keys have been pressed this
     -- frame, to get around the fact that LÖVE's default callback won't let us
@@ -199,6 +202,45 @@ function love.draw()
     displayFPS()
 
     push:apply('end')
+end
+
+function loadHighScores()
+  love.filesystem.setIdentity('breakout')
+
+  if not love.filesystem.getInfo('breakout.lst') then
+    local scores = ''
+    for i = 10, 1, -1 do
+      scores = scores .. 'SLS\n'
+      scores = scores .. tostring(i * 1000) .. '\n'
+    end
+
+    love.filesystem.write('breakout.lst', scores)
+  end
+
+  local name = true
+  local currentName = nil
+  local counter = 1
+
+  local scores = {}
+  for i = 1, 10 do
+    scores[i] = {
+      name = nil,
+      score = nil
+    }
+  end
+
+  for line in love.filesystem.lines('breakout.lst') do
+    if name then
+      scores[counter].name = string.sub(line, 1, 3)
+    else
+      scores[counter].score = tonumber(line)
+      counter = counter + 1
+    end
+
+    name = not name
+  end
+
+  return scores
 end
 
 function renderHealth(health)
